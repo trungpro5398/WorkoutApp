b733151093bf1dd6e22674d563cd170806c4478a
quangtrungnguyen <trungpro5398@gmail.com>
2023-05-05 23:01:31 +1000

    Add navigation drawer

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.workoutapp;
 
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -14,11 +18,14 @@ import com.example.workoutapp.fragment.SearchFragment;
 import com.example.workoutapp.viewmodel.WorkoutViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.workoutapp.entity.Workout;
 import com.example.workoutapp.retrofit.YouTubeApiService;
 import com.example.workoutapp.retrofit.YouTubeResponse;
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -40,15 +47,60 @@ public class MainActivity extends AppCompatActivity {
 
         workoutViewModel = new ViewModelProvider(this).get(WorkoutViewModel.class);
 
-        // Set up the bottom navigation view
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle navigation view item clicks here.
+                int id = item.getItemId();
+
+                Fragment selectedFragment = null;
+
+                switch (id) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_analytical:
+                        selectedFragment = new AnalyticalFragment();
+                        break;
+                    case R.id.nav_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.nav_calendar:
+                        selectedFragment = new CalendarFragment();
+                        break;
+                    case R.id.nav_map:
+                        selectedFragment = new MapFragment();
+                        break;
+                    // Add more cases for other menu items if needed
+                }
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        // Set the default fragment to HomeFragment
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, new HomeFragment())
                 .commit();
-        // Fetch workouts and insert them into the database using Retrofit
+
         fetchWorkouts();
     }
 
@@ -91,9 +143,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
 
     // Bottom navigation item selection listener
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
--- app/src/main/java/com/example/workoutapp/fragment/SearchFragment.java
+++ app/src/main/java/com/example/workoutapp/fragment/SearchFragment.java
@@ -1,42 +1,42 @@
 package com.example.workoutapp.fragment;
 
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.workoutapp.R;
 import com.example.workoutapp.adapter.WorkoutCategoryAdapter;
+import com.example.workoutapp.databinding.SearchFragmentBinding;
 import com.example.workoutapp.viewmodel.WorkoutViewModel;
-
 
 public class SearchFragment extends Fragment {
 
     private WorkoutViewModel workoutViewModel;
     private WorkoutCategoryAdapter adapter;
+    private SearchFragmentBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.search_fragment, container, false);
+        binding = SearchFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        RecyclerView recyclerView = view.findViewById(R.id.workout_recycler_view);
         adapter = new WorkoutCategoryAdapter();
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.workoutRecyclerView.setAdapter(adapter);
+        binding.workoutRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
 
         workoutViewModel = new ViewModelProvider(requireActivity()).get(WorkoutViewModel.class);
         workoutViewModel.getDistinctWorkoutTypes().observe(getViewLifecycleOwner(), workoutTypes -> {
@@ -54,8 +54,7 @@ public class SearchFragment extends Fragment {
                     .commit();
         });
 
-        SearchView searchView = view.findViewById(R.id.search_view);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
@@ -67,5 +66,11 @@ public class SearchFragment extends Fragment {
                 return true;
             }
         });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
