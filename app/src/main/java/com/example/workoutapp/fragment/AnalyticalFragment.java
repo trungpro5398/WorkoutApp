0d397c4e2e6847f266448f09d1df897255f54045
irad2 <irad2@student.monash.edu>
2023-05-10 19:43:53 +1000

    Analytical stuff

--- app/src/main/java/com/example/workoutapp/fragment/AnalyticalFragment.java
+++ app/src/main/java/com/example/workoutapp/fragment/AnalyticalFragment.java
@@ -5,11 +5,14 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.viewmodel.CreationExtras;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,6 +34,7 @@ import com.kizitonwose.calendar.view.ViewContainer;
 import com.kizitonwose.calendar.view.WeekCalendarView;
 import com.kizitonwose.calendar.view.WeekDayBinder;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,14 +43,16 @@ public class AnalyticalFragment extends Fragment {
     private WorkoutRecordViewModel workoutRecordViewModel;
     private WorkoutRecordAdapter adapter;
     private AnalyticalFragmentBinding binding;
-    private List<WorkoutRecord> recordList;
+    private List<WorkoutRecord> recordList = new ArrayList<>();
 
     private WeekCalendarView calendarView;
+
+    //private int duration;
 
     // Pie chart data
     private List<PieEntry> entries = new ArrayList<>();
 
-    public AnalyticalFragment(){
+    public AnalyticalFragment() {
         entries.add(new PieEntry(18.5f, "Green"));
         entries.add(new PieEntry(26.7f, "Yellow"));
         entries.add(new PieEntry(24.0f, "Red"));
@@ -77,39 +83,108 @@ public class AnalyticalFragment extends Fragment {
 //
 //            }
 //        });
-        return binding.getRoot();
+        View view = binding.getRoot();
+        adapter = new WorkoutRecordAdapter();
+        binding.workoutRecordRecyclerView.setAdapter(adapter);
+        binding.workoutRecordRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        workoutRecordViewModel = new ViewModelProvider(requireActivity()).get(WorkoutRecordViewModel.class);
+
+        /*
+        This observer is just for the recycler view
+         */
+        workoutRecordViewModel.getAllWorkoutRecords().observe(getViewLifecycleOwner(), workoutRecords -> {
+                    adapter.setWorkoutRecords(workoutRecords);
+                }
+
+        );
+        /*
+        The calendarSelect method is the method that you'll be working on for everything i assume
+         */
+        calendarSelect();
+        return view;
 
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        adapter = new WorkoutRecordAdapter();
-        binding.workoutRecordRecyclerView.setAdapter(adapter);
-        binding.workoutRecordRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-        workoutRecordViewModel = new ViewModelProvider(requireActivity()).get(WorkoutRecordViewModel.class);
-        workoutRecordViewModel.getAllWorkoutRecords().observe(getViewLifecycleOwner(), workoutRecords -> {
-            adapter.setWorkoutRecords(workoutRecords);
-            String duration = workoutRecordViewModel.getWorkoutDurationForDate("2023-05-08");
-            String dailyDurationGoal = "20";
-            binding.tvDuration.setText(duration+ "/"+ dailyDurationGoal + " min");
-            }
-
-        );
-//        String duration = workoutRecordViewModel.getWorkoutDurationForDate("2023-05-08");
-//        String dailyDurationGoal = "20";
-
-
-
-
-
 
     }
+
+
     @NonNull
     @Override
     public CreationExtras getDefaultViewModelCreationExtras() {
         return super.getDefaultViewModelCreationExtras();
     }
+
+    public void calendarSelect() {
+
+        binding.calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+
+            /*
+            The function that triggers when the date is changed.
+             */
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int y, int m, int d) {
+                String selectedDate = String.format("%d-%02d-%02d", y, m + 1, d);
+
+
+                // The fancy observer model for this DO NOT EDIT
+                workoutRecordViewModel.getAllWorkoutRecords().observe(getViewLifecycleOwner(), new Observer<List<WorkoutRecord>>() {
+
+                    @Override
+                    public void onChanged(@Nullable final List<WorkoutRecord> workoutRecords) {
+                        /*
+                        INSIDE HERE is where I think you'll be doing all the work.
+                        Call all of your setters for the dashboards from inside here
+                        You'll probably have to declare an int calories = 0 right here
+                         */
+                        int duration = 0;
+                        for (WorkoutRecord r: workoutRecords) {
+
+                            if (r.getWorkoutDate().equals(selectedDate) ) {
+                                String d = r.getWorkoutDuration();
+                                /*
+                                Past here is the bit where each record is checked by date
+                                so you'll prob be putting in the logic for the calorie calculation.
+                                 */
+
+                                if (d.contains("hr")) {
+                                    int nd = Integer.valueOf(d.replace("hr", ""));
+                                    nd = nd * 60;
+                                    duration = duration + nd;
+                                }
+                                else if (d.contains("min")) {
+                                    int nd = Integer.valueOf(d.replace("min", ""));
+                                    duration = duration + nd;
+                                }
+
+                                /*
+                                For example
+                                String t = r.getWorkoutType();
+
+                                if (t.equals("Running") {
+                                    calories = hourlyRunningCals * (nd/60)
+                                    }
+                                etc etc
+                                 */
+                            }
+
+                        }
+                        String dailyDurationGoal = "20";
+                        binding.tvDuration.setText(duration + "/" + dailyDurationGoal + " min");
+
+                        /*
+                        This is where you'll be doing the setters for the progress bar things same as
+                        I did for tvDuration
+                         */
+                    }});
+    }
+
+     });
+    }
 }
+
 
 //public class DayViewContainer  {
 //     TextView textView;
